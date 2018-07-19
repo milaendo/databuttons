0272266b22892971d92dbaf7588bc4deede86cd6
Mila Endo <milal.alim@gmail.com>
2018-07-19 14:33:48 -0700

    passing data from parent to child and rendered with a data table

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,57 @@
   <div class="hello">
     <h2>Im the nested child with the click event</h2>
     <button @click="onCLick">Click</button>
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">code</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody v-for="item,index in data">
+    <tr>
+      <td>{{item.name}}</td>
+      <td>{{item.code}}</td>
+      <td>{{item.description}}</td>
+    </tr>
+  </tbody>
+</table>
+    <!-- <b-modal ref="myModalRef" hide-footer title="Using Component Methods">
+    <div class="d-block text-center">
+      <b-form v-if="show">
+      <b-form-group id="exampleInputGroup1"
+                    label=""
+                    label-for="exampleInput1">
+        <b-form-input id="exampleInput1"
+                      type="email"
+                      required
+                      :placeholder="user.name">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup2"
+                    label=""
+                    label-for="exampleInput2">
+        <b-form-input id="exampleInput2"
+                      type="text"
+                      required
+                      :placeholder="user.code">
+        </b-form-input>
+      </b-form-group>
+    </b-form>
+    </div>
+    <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-btn>
+  </b-modal> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  props: ['data'],
   data () {
     return {
-
+      show: true,
     }
   },
   created() {
@@ -18,7 +60,14 @@ export default {
   },
   methods: {
     onCLick() {
-      console.log('ive been clicked')
+      this.showModal()
+      console.log('title', this.data)
+    },
+    showModal () {
+      this.$refs.myModalRef.show()
+    },
+    hideModal () {
+      this.$refs.myModalRef.hide()
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,106 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  data: {
-       "data":[
-          {
-             "uid":"7168a8bd-dfc3-48c3-83f5-e696781924ac",
-             "parent_role_id":null,
-             "name":"name",
-             "description":"desr",
-             "code":"code",
-             "created_at":"2018-07-17 03:05:49",
-             "updated_at":"2018-07-18 23:45:54",
-             "status":"ACTIVE",
-             "total_users":0
-          },
-          {
-             "uid":"7663b157-d673-4000-9fa9-33e5922c9bff",
-             "parent_role_id":null,
-             "name":"nice",
-             "description":"new",
-             "code":"hello",
-             "created_at":"2018-07-17 03:05:49",
-             "updated_at":"2018-07-18 23:44:28",
-             "status":"DISABLED",
-             "total_users":0
-          },
-          {
-             "uid":"1eeb3937-fcca-4a7c-aadc-df09cbe20302",
-             "parent_role_id":null,
-             "name":"Administrator",
-             "description":"Overall Administration of System",
-             "code":"PROCESSMAKER_ADMIN",
-             "created_at":"2018-06-27 16:14:37",
-             "updated_at":"2018-06-27 16:14:37",
-             "status":"ACTIVE",
-             "total_users":1
-          },
-          {
-             "uid":"b6679b4f-44f0-4ec2-9d1d-c5358dc32c1b",
-             "parent_role_id":null,
-             "name":"Manager",
-             "description":"Allow management of cases,users and groups.",
-             "code":"PROCESSMAKER_MANAGER",
-             "created_at":"2018-06-27 16:14:37",
-             "updated_at":"2018-06-27 16:14:37",
-             "status":"ACTIVE",
-             "total_users":0
-          },
-          {
-             "uid":"239b6126-a52e-411a-994e-cb08aea85b20",
-             "parent_role_id":null,
-             "name":"Operator",
-             "description":"Standard Users that allow login and execution of cases",
-             "code":"PROCESSMAKER_OPERATOR",
-             "created_at":"2018-06-27 16:14:37",
-             "updated_at":"2018-06-27 16:14:37",
-             "status":"ACTIVE",
-             "total_users":0
-          },
-          {
-             "uid":"20f871d5-5495-41a4-a90a-f6c938d58953",
-             "parent_role_id":null,
-             "name":"hh",
-             "description":"kk",
-             "code":"wqdefrdfg",
-             "created_at":"2018-07-18 21:14:37",
-             "updated_at":"2018-07-18 23:07:14",
-             "status":"DISABLED",
-             "total_users":0
-          },
-          {
-             "uid":"c5c12b18-9f19-48a4-9628-8e4d568ab6d4",
-             "parent_role_id":null,
-             "name":"name",
-             "description":"yes",
-             "code":"yyyyy",
-             "created_at":"2018-07-18 21:51:19",
-             "updated_at":"2018-07-18 23:35:17",
-             "status":"DISABLED",
-             "total_users":0
-          }
-       ],
-       "meta":{
-          "total":7,
-          "count":7,
-          "per_page":10,
-          "current_page":1,
-          "total_pages":1,
-          "filter":null,
-          "sort_by":"",
-          "sort_order":""
-       }
-    },
-  router,
   components: { App },
   template: '<App/>',
   created() {
